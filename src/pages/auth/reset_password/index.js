496d23f12b617fce5decb83023d555a530b39312
ronaldscruz <ronald.scruz16@gmail.com>
2019-03-15 15:07:00 -0300

    funçao reset_password quase concluida

--- src/pages/auth/reset_password/index.js
+++ src/pages/auth/reset_password/index.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
+
 import '../style.css'
+
+import axios from 'axios'
 
 import {
    Form, Button,
@@ -9,9 +12,44 @@ import {
    FaKey, FaUserAlt, FaStar, FaAt
 } from 'react-icons/fa'
 
+import Info from '../../../components/Info'
+
 export default class ResetPassword extends Component{
+   constructor(props){
+      super(props)
+
+      this.state = {
+         infoType:'',
+         infoReport: '',
+         token: ''
+      }
+      this.resetPwd = this.resetPwd.bind(this)
+   }
+
    componentDidMount(){
       document.title = "Reset Password"
+   }
+
+   resetPwd(e){
+      e.preventDefault()
+
+      const form = Array.from(e.target.elements).map(inp => inp.value)
+      const [cEmail, newPwd, cNewPwd] = form
+
+      if(newPwd !== cNewPwd)
+         return this.setState({infoReport: 'Passwords must match!'})
+      
+      try{
+         const response = axios.post(`http://auth-api-nodejs.herokuapp.com/auth/reset_password/${this.props.match.params.token}`, {
+            cEmail, newPwd
+         })
+
+         this.setState({infoReport: response.data.ok, infoType: 'success'})
+
+         return this.props.history.push("/")
+      }catch(err){
+         return this.setState({infoReport: err.response.data.error, infoType: 'error'})
+      }
    }
 
    render(){
@@ -20,7 +58,8 @@ export default class ResetPassword extends Component{
             <div className="form-wrapper">
                <h4> <FaUserAlt/> Reset password</h4>
                <hr></hr>
-               <Form method="post" name="reset_password">
+               <Info infoType={this.state.infoType} msg={this.state.infoReport} />
+               <Form method="post" name="reset_password" onSubmit={this.resetPwd}>
                   <Form.Group controlId="email">
                      <Form.Label><FaAt/> Confirm email </Form.Label>
                      <Form.Control type="email" name="email" placeholder="example@email.com" required></Form.Control>
@@ -30,10 +69,10 @@ export default class ResetPassword extends Component{
                      <Form.Control type="password" name="password" placeholder="********" required></Form.Control>
                   </Form.Group>
                   <Form.Group controlId="password">
-                     <Form.Label><FaKey/> Confirm password</Form.Label>
+                     <Form.Label><FaKey/> Confirm new password</Form.Label>
                      <Form.Control type="password" name="password" placeholder="********" required></Form.Control>
                   </Form.Group>
-                  <Button variation="primary" className="btn-block"><FaStar/> Reset</Button>
+                  <Button variation="primary" className="btn-block" type="submit"><FaStar/> Reset</Button>
                </Form>
             </div>
          </div>
