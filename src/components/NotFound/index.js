ef96128b9838416859cd017da44dfabf5f84a4ac
ronaldscruz <ronald.scruz16@gmail.com>
2019-03-15 15:05:14 -0300

    componente NotFound criado

---
+++ src/components/NotFound/index.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import './style.css'
+
+import { Button } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import { FaRobot } from 'react-icons/fa'
+
+class NotFound extends Component {
+   componentDidMount(){
+      document.title = "404"
+   }
+
+   render() { 
+      return ( 
+         <div className="wrapper">
+            <div className="window">
+               <div className="window-header">
+                  <h3><FaRobot/>Oops!</h3>
+                  <Link to="/"><Button variant="warning" size="sm"> ⮜ Home </Button></Link>
+               </div>    
+                  <hr></hr>
+               <h5> Page not found :( </h5>
+            </div>
+         </div>
+       );
+   }
+}
+ 
+export default NotFound;
