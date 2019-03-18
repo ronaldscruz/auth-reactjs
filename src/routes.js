96482f759015176939b8c201a89fe53917a0468f
ronaldscruz <ronald.scruz16@gmail.com>
2019-03-18 16:02:57 -0300

    rotas do painel administrativo adicionadas

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 // Misc
 import NotFound from './components/NotFound'
+import Unauthorized from './components/Unauthorized'
 
 // Auth
 import Auth from './pages/auth'
@@ -11,17 +12,25 @@ import ForgotPassword from './pages/auth/forgot_password'
 
 // Panel
 import NewsPanel from './pages/manage_news'
+import NewsView from './pages/manage_news/read'
+import NewsUpdate from './pages/manage_news/create'
+import NewsCreate from './pages/manage_news/update'
 
 const Routes = () => (
    <BrowserRouter>
       <Switch>
          <Route exact path="/" component={Auth} />
          <Route exact path="/forgot_password" component={ForgotPassword} />
-         <Route path="/reset_password/:token/:userId" component={ResetPassword} />
-         <Route exact path="/manage_news" component={NewsPanel}>
-         </Route>
+         <Route exact path="/reset_password/:token/:userId" component={ResetPassword} />
+         
+         <Route exact path="/manage_news" component={NewsPanel}/>
+         <Route exact path="/manage_news/:newsId" component={NewsView}/>
+         <Route exact path="/manage_news/update/:newsId" component={NewsUpdate}/>
+         <Route exact path="/manage_news/create" component={NewsCreate}/>
 
-         <Route component={NotFound}/>
+         <Route exact path="/401" component={Unauthorized}/>
+
+         <Route component={NotFound}/>        
       </Switch>
    </BrowserRouter>
 )
