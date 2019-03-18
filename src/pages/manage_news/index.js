187840f0dd660277727b0cfbbd78ae3f8b33a448
ronaldscruz <ronald.scruz16@gmail.com>
2019-03-18 15:58:38 -0300

    display:grid do manage_news modelado

--- src/pages/manage_news/index.js
+++ src/pages/manage_news/index.js
@@ -1,25 +1,48 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
+import './style.css'
+
+import ControlMenu from '../../components/ControlMenu'
+import Footer from '../../components/Footer'
+
 export default class NewsPanel extends Component {
    constructor(props) {
       super(props);
-      this.state = {  }
+      this.state = { 
+         authorized: false
+      }
    }
 
-   async componentDidMount(){
-      const response = await axios.get('http://auth-api-nodejs.herkouapp.com/manage_news', {
-         headers: {
-            "Authorization": localStorage.getItem('token')
-         }
-      })
+   async componentWillMount(){
+      document.title = "Manage News"
 
-      console.log(response)
+      try{
+         await axios.get('http://auth-api-nodejs.herokuapp.com/manage_news', {
+            headers: {
+               "Authorization": localStorage.getItem('token')
+            }
+         })
+
+         return this.setState({authorized: true})
+      }catch(err){
+         return this.props.history.push('/401')
+      }
    }
 
    render() { 
       return (
-         <h1>LOGUEI?</h1>
+         this.state.authorized === true &&
+         <React.Fragment>
+            <ControlMenu/>
+            <div className="content-wrapper">
+               <div className="display">
+                  eae
+               </div>
+
+               <Footer/>
+            </div>
+         </React.Fragment>
       );
    }
 }
