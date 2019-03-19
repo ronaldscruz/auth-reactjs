837a0fd9fee30318673c799b6dc851e0f9a57f9d
ronaldscruz <ronald.scruz16@gmail.com>
2019-03-18 21:57:16 -0300

    'Show all' concluido

--- src/pages/manage_news/index.js
+++ src/pages/manage_news/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
 import axios from 'axios';
 
 import './style.css'
@@ -6,12 +7,15 @@ import './style.css'
 import ControlMenu from '../../components/ControlMenu'
 import Footer from '../../components/Footer'
 
+import { Button } from 'react-bootstrap'
+import { FaEye, FaNewspaper } from 'react-icons/fa'
+
 export default class NewsPanel extends Component {
    constructor(props) {
       super(props);
       this.state = { 
          authorized: false,
-
+         page: 1,
          // Array that stores all news
          allNewsData: []
       }
@@ -21,15 +25,15 @@ export default class NewsPanel extends Component {
    async componentWillMount(){
       document.title = "Manage News"
 
-      // Verify if there is a valid auth token
+      // Verify if there is a valid auth token and retrieve news data
       try{
-         const response = await axios.get('http://auth-api-nodejs.herokuapp.com/manage_news', {
+         const response = await axios.get(`http://auth-api-nodejs.herokuapp.com/manage_news?page=${this.state.page}`, {
             headers: {
                "Authorization": localStorage.getItem('token')
             }
          })
 
-         return this.setState({authorized: true})
+         return this.setState({authorized: true, allNewsData: response.data.docs})
       }catch(err){
          return this.props.history.push('/401')
       }
@@ -43,9 +47,22 @@ export default class NewsPanel extends Component {
             <ControlMenu/>
             <div className="content-wrapper">
                <div className="display">
-                  eae
+               <h2 className="main-header"> <FaNewspaper/> Manage news</h2>
+                  <section className="news-box-wrapper">
+                     <ul className="news-list">
+                        {this.state.allNewsData.map((news)=> (
+                           <li className="news-element">
+                              <p className="news-title">{news.title}</p>
+                              <p className="news-lead">{news.lead}</p>
+                              <div className="news-element-footer">
+                                 <p className="news-author">Author: {news.author.name}</p>
+                                 <Link to={`/manage_news/${news.id}`} ><Button variant="primary" size="md"> <FaEye/> View more</Button></Link>
+                              </div>
+                           </li>
+                        ))}
+                     </ul>
+                  </section>
                </div>
-
                <Footer/>
             </div>
          </React.Fragment>
